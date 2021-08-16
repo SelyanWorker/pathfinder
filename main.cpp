3ee0eded554a54515ccbeb21a9f696c359b984db
Selyan <selyanworker@gmail.com>
2021-08-17 00:33:40 +0500

    Disable scroll bars

--- main.cpp
+++ main.cpp
@@ -8,10 +8,55 @@
 #include <QLabel>
 #include <QGridLayout>
 
-namespace utils
+class MainWidget : public QWidget
 {
+    Q_OBJECT
+
+public:
+    MainWidget(QWidget *parent = nullptr)
+    :   QWidget(parent)
+    {
+        m_scene = new QGraphicsScene;
+        fill_scene(*m_scene, { 40, 40 }, { 2, 2 });
+
+        m_view = new QGraphicsView(m_scene);
+        //view->scale(2, 2);
+
+        m_view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff); // Отключим скроллбар по горизонтали
+        m_view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);   // Отключим скроллбар по вертикали
+
+        m_width = new QSpinBox;
+        m_width->setRange(0, 100);
+        m_height = new QSpinBox;
+        m_height->setRange(0, 100);
+
+        QPushButton *generateButton = new QPushButton{ "Generate" };
+        connect(generateButton,
+                QPushButton::pressed,
+                [&]()
+                {
+                    fill_scene(*m_scene, { 40, 40 }, QSize{ m_width->value(), m_height->value() });
+                });
+
+        QHBoxLayout *sizeInput = new QHBoxLayout;
+        sizeInput->addWidget(new QLabel{ "w:" });
+        sizeInput->addWidget(m_width);
+        sizeInput->addWidget(new QLabel{ "h:" });
+        sizeInput->addWidget(m_height);
+
+        QGridLayout* mainLayout = new QGridLayout;
+        mainLayout->addWidget(m_view);
+        mainLayout->addLayout(sizeInput, 1, 0);
+        mainLayout->addWidget(generateButton);
+
+        setLayout(mainLayout);
+    }
+
+private slots:
     void fill_scene(QGraphicsScene& scene, const QSize& rectSize, const QSize& size)
     {
+        scene.clear();
+
         QPointF lastRect{ 0, 0 };
 
         for(size_t i = 0; i < size.height(); ++i)
@@ -25,42 +70,17 @@ namespace utils
             lastRect.ry() += rectSize.height();
         }
     }
-}
 
-class MainWidget : public QWidget
-{
-    Q_OBJECT
-
-public:
-    MainWidget(QWidget *parent = nullptr)
-    :   QWidget(parent)
+    void scale_scene(qreal scale)
     {
-        QGraphicsScene* scene = new QGraphicsScene;
-        utils::fill_scene(*scene, { 40, 40 }, { 2, 2 });
 
-        QGraphicsView* view = new QGraphicsView(scene);
-
-        QSpinBox* w = new QSpinBox;
-        w->setRange(0, 100);
-        QSpinBox* h = new QSpinBox;
-        h->setRange(0, 100);
-
-        QPushButton* generateButton = new QPushButton{ "Generate" };
-
-        QHBoxLayout* sizeInput = new QHBoxLayout;
-        sizeInput->addWidget(new QLabel{ "w:" });
-        sizeInput->addWidget(w);
-        sizeInput->addWidget(new QLabel{ "h:" });
-        sizeInput->addWidget(h);
-
-        QGridLayout* mainLayout = new QGridLayout;
-        mainLayout->addWidget(view);
-        mainLayout->addLayout(sizeInput, 1, 0);
-        mainLayout->addWidget(generateButton);
-
-        setLayout(mainLayout);
     }
 
+private:
+    QGraphicsScene* m_scene;
+    QGraphicsView* m_view;
+    QSpinBox* m_width;
+    QSpinBox* m_height;
 };
 
 int main(int argc, char **argv)
